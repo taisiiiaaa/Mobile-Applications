1d5d593999f87c08fde99cacbd9a90f2943b4363
Taisiia Shloma <shloma20082004@icloud.com>
2024-10-29 21:57:36 +0100

    fixed some IDs' names etc

--- app/src/main/java/com/example/registrationforms/MainActivity.kt
+++ app/src/main/java/com/example/registrationforms/MainActivity.kt
@@ -6,13 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-// Cred change
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.first_form)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
