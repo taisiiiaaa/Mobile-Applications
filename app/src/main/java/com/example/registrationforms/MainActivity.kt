0f13fc996a9a1afb27ae4a2e8c05625bb50be2c8
Taisiia Shloma <shloma20082004@icloud.com>
2024-11-10 14:14:56 +0100

    finished the TextInputLayout when clicking on the "Register now"/"Log in", it navigates to the other activity organised the backstack so that the activity is always the single and only member of its task

--- app/src/main/java/com/example/registrationforms/MainActivity.kt
+++ app/src/main/java/com/example/registrationforms/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.registrationforms
 
+import android.content.Intent
+import android.credentials.RegisterCredentialDescriptionRequest
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.RegisterReceiverFlags
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,37 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val registerNowLabel = findViewById<TextView>(R.id.register)
+
+        registerNowLabel.setOnClickListener {
+            Log.d("Onboarding", "Clicked register now label")
+
+            val goToRegisterIntent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(goToRegisterIntent)
+        }
+    }
+}
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val logInLabel = findViewById<TextView>(R.id.log_in)
+
+        logInLabel.setOnClickListener {
+            Log.d("Onboarding", "Clicked log in label")
+
+            val goToLogInIntent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(goToLogInIntent)
         }
     }
 }
